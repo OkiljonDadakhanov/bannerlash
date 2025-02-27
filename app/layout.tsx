77046ad0dd32f3d9836148dd0f1b67c22720b47d
Okiljon <okiljondadakhonov@gmail.com>
2025-02-27 18:06:39 +0500

    analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/next';
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -28,7 +29,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Theme>{children}</Theme>
+        <Theme>
+          {children}
+        <Analytics />
+        </Theme>
       </body>
     </html>
   );
